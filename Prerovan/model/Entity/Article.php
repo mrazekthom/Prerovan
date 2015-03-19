0757451c9601645170f81092b88966722d97c544
Mrázek Tomáš <mrazekthom@gmail.com>
2015-03-19 21:26:06 +0100

    Big pig!

---
+++ Prerovan/model/Entity/Article.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Prerovan\Model\Entity;
+
+use LeanMapper\Entity;
+
+
+/**
+ * @property int       $id          testasf
+ * @property string    $URL
+ * @property string    $section
+ * @property string    $title
+ * @property string    $content
+ * @property string    $writer
+ * @property boolean   $conformed
+ * @property \Datetime $inserted
+ */
+class Article extends Entity
+{
+
+}
