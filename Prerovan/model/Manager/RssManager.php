17695c8b36c41f2e66136bebca28c4d9f60081af
Mrázek Tomáš <mrazekthom@gmail.com>
2015-03-24 17:24:52 +0100

    add RssFactory and delete Rssfeed table

--- Prerovan/model/Manager/RssManager.php
+++ Prerovan/model/Manager/RssManager.php
@@ -2,51 +2,23 @@
 
 namespace Prerovan\Model\Manager;
 
-
 use Nette\Object;
-use Kdyby\Curl;
-use Prerovan\Model\Repository\RssfeedRepository;
+use Prerovan\Model\Factory\RssFactory;
 
 class RssManager extends Object
 {
 
-    /** @var  RssfeedRepository */
-    private $RR;
+    /** @var RssFactory */
+    private $RF;
 
-    public function __construct(RssfeedRepository $RR){
-        $this->RR = $RR;
-    }
-
-    private function rssSlugByCategory($RssCategory)
+    public function __construct(RssFactory $RF)
     {
-        $rssSlug = $this->RR->getRssSlug($RssCategory);
-        return $rssSlug;
+        $this->RF = $RF;
     }
 
     public function getRss($RssCategory)
     {
-        $url = $this->rssSlugByCategory($RssCategory);
-        $rssFeed = [];
-        $request = new Curl\Request($url);
-        try {
-            $response = $request->get();
-            $header = $response->getHeaders();
-            $response = $response->getResponse();
-        } catch (Curl\CurlException $e) {
-            dump($e->getMessage());
-        }
-        #$xml = simplexml_load_string($response) or die("Error: Cannot create object");
-        $xml = new \SimpleXMLElement($response);
-        $bar = $xml->getNamespaces(true);
-        for ($i = 0; $i <= 5; $i++){
-            if (isset($xml->channel->item[$i]->children($bar['szn'])->image)){
-                $rssFeed[] = [
-                    'title' => (string)$xml->channel->item[$i]->title,
-                    'url' => (string)$xml->channel->item[$i]->link,
-                    'image' => (string)$xml->channel->item[$i]->children($bar['szn'])->image
-                ];
-            }
-        }
+        $rssFeed = $this->RF->generate($RssCategory);
         return $rssFeed;
     }
 
