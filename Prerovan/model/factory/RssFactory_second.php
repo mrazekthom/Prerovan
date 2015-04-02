a956e1adf069144f2a6008152c29e69a3f4eca00
mrazekthom <mrazekthom@gmail.com>
2015-04-02 19:24:54 +0200

    Create RssFactory_second.php

---
+++ Prerovan/model/factory/RssFactory_second.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Prerovan\Model\Factory;
+
+use Kdyby\Curl;
+use Nette\Object;
+
+class RssFactory extends Object
+{
+
+    const NEWS = "http://www.aktualne.cz/mrss/";
+    const SPORT = "http://www.sport.cz/rss2";
+    const BOULEVARD = "http://www.super.cz/rss2";
+    const STREAM = "";
+
+    public function generate($rssFeed)
+    {
+
+        $url = $rssFeed;
+        $rssNewsFeed = [];
+        $request = new Curl\Request($url);
+        try {
+            $response = $request->get();
+            $header = $response->getHeaders();
+            $response = $response->getResponse();
+        } catch (Curl\CurlException $e) {
+            throw $e;
+
+        }
+        $xml =  new \SimpleXMLElement($response);
+
+        if ($rssFeed == static::NEWS) {
+            return $this->generateNewsFeed($xml);
+        } elseif ($rssFeed == static::SPORT) {
+            return $this->generateSportFeed($xml);
+        } elseif ($rssFeed == static::BOULEVARD) {
+            return $this->generateBoulevardFeed($xml);
+        } elseif ($rssFeed == static::STREAM) {
+            return $this->generateVideoFeed($xml);
+        }
+    }
+
+    private function generateNewsFeed($xml)
+    {
+        $namespace = $xml->getNamespaces(true);
+        for ($i = 0; $i <= 2; $i++) {
+            $rssFeed[] = [
+                'title' => (string)$xml->channel->item[$i]->title,
+                'url' => (string)$xml->channel->item[$i]->link,
+                'description' => (string)$xml->channel->item[$i]->description,
+                'image' => (string)$xml->channel->item[0]->children($namespace['media'])->group->children($namespace['media'])->content->attributes()->url
+            ];
+        }
+        $rssNewFeed['rssFeed'] = $rssFeed;
+        $rssNewFeed['name'] = 'Aktuálně';
+        return $rssNewFeed;
+    }
+
+    private function generateSportFeed($xml)
+    {
+        $namespace = $xml->getNamespaces(true);
+        for ($i = 0; $i <= 2; $i++) {
+            $rssSportFeed[] = [
+                'title' => (string)$xml->channel->item[$i]->title,
+                'url' => (string)$xml->channel->item[$i]->link,
+                'description' => (string)$xml->channel->item[$i]->description,
+                'image' => (string)$xml->channel->item[$i]->children($namespace['szn'])->image
+            ];
+        }
+        return $rssSportFeed;
+    }
+
+
+    private function generateBoulevardFeed($xml)
+    {
+        $namespace = $xml->getNamespaces(true);
+        for ($i = 0; $i <= 2; $i++) {
+            $rssBoulevardFeed[] = [
+                'title' => (string)$xml->channel->item[$i]->title,
+                'url' => (string)$xml->channel->item[$i]->link,
+                'description' => (string)$xml->channel->item[$i]->description,
+                'image' => (string)$xml->channel->item[$i]->children($namespace['szn'])->image
+            ];
+        }
+        return $rssBoulevardFeed;
+    }
+
+    private function generateVideoFeed($xml)
+    {
+
+    }
+
+}
