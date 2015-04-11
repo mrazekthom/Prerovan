ce0d8a9b69b3bc5e470392dd44687e83ed7c2668
Mrázek Tomáš <mrazekthom@gmail.com>
2015-04-11 17:02:00 +0200

    added Survey database

--- Prerovan/components/SurveyComponent/SurveyComponent.php
+++ Prerovan/components/SurveyComponent/SurveyComponent.php
@@ -2,11 +2,25 @@
 
 namespace Prerovan\Components;
 
+use Prerovan\Model\Repository\SurveyRepository;
+
 class SurveyComponent extends BaseComponent
 {
 
+    /** @var  SurveyRepository */
+    private $SR;
+
+    public function __construct(SurveyRepository $SR){
+        $this->SR = $SR;
+    }
+
     public function render()
     {
+        foreach ($this->SR->ss() as $item) {
+            $name = $item->surveyCategory->name;
+        }
+        $this->template->name = $name;
+        $this->template->survey = $this->SR->ss();
         $this->template->render();
     }
 
--- Prerovan/model/Entity/survey.php
+++ Prerovan/model/Entity/survey.php
@@ -9,7 +9,7 @@ use LeanMapper\Entity;
  * @property int                  $id
  * @property string               $name
  * @property integer              $counter
- * @property SurveyCategory       $surveyCategory
+ * @property SurveyCategory       $surveyCategory m:hasOne
  */
 class Survey extends Entity
 {
